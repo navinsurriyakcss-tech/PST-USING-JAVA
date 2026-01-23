d90f11ee0b59eee62a4636f32bb39e0578a38852
NAVIN SURRIYA K <navinsurriyak.css@gmail.com>
2026-01-23 09:06:28 +0530

    Create Sortcgpa.java

---
+++ Sortcgpa.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+class Student {
+    private int id;
+    private String fname;
+    private double cgpa;
+
+    public Student(int id, String fname, double cgpa) {
+        this.id = id;
+        this.fname = fname;
+        this.cgpa = cgpa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public double getCgpa() {
+        return cgpa;
+    }
+}
+
+public class Sortcgpa {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        List<Student> students = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int id = in.nextInt();
+            String name = in.next();
+            double cgpa = in.nextDouble();
+            students.add(new Student(id, name, cgpa));
+        }
+
+        Collections.sort(students, (a, b) -> {
+            if (a.getCgpa() != b.getCgpa()) {
+                return Double.compare(b.getCgpa(), a.getCgpa());
+            } else if (!a.getFname().equals(b.getFname())) {
+                return a.getFname().compareTo(b.getFname()); 
+            } else {
+                return Integer.compare(a.getId(), b.getId()); 
+            }
+        });
+
+    
+        for (Student s : students) {
+            System.out.println(s.getFname());
+        }
+    }
+}

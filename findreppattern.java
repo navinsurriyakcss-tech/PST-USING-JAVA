da400beccc06719832f84b59df84765e632ac0a5
NAVIN SURRIYA K <navinsurriyak.css@gmail.com>
2026-02-27 09:52:28 +0530

    Create findreppattern.java

---
+++ findreppattern.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class findreppattern {
+
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+        List<String> result = new ArrayList<>();
+
+        for (String word : words) {
+            if (matches(word, pattern)) {
+                result.add(word);
+            }
+        }
+
+        return result;
+    }
+
+    private boolean matches(String word, String pattern) {
+
+        if (word.length() != pattern.length()) return false;
+
+        HashMap<Character, Character> map = new HashMap<>();
+        HashSet<Character> used = new HashSet<>();
+
+        for (int i = 0; i < word.length(); i++) {
+            char p = pattern.charAt(i);
+            char w = word.charAt(i);
+
+            if (map.containsKey(p)) {
+                if (map.get(p) != w) return false;
+            } else {
+                if (used.contains(w)) return false;
+
+                map.put(p, w);
+                used.add(w);
+            }
+        }
+
+        return true;
+    }
+}
